06a8a65e1b44e4f2e7af73ab332344a7b3a93f11
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-07 23:37:30 -0300

    ✨ Remove DB connection

--- packages/server/index.js
+++ packages/server/index.js
@@ -18,41 +18,43 @@ const PORT = port(NODE_ENV)
 import Router from 'Routes'
 
 // load DB connection
-import db from 'Database'
+// import db from 'Database'
 
 import logger from '@macroeconomia/logger'
 logger(123)
 
-db.sync().then(() => {
-  app.use('/api', Router({ db }))
+// db.sync().then(() => {
 
-  // create a route for the app
-  app.get('/api', (req, res) => {
-    if (NODE_ENV !== "production") {
-      res.json({
-        message: "this is my starter project for a Node.js API with a postgres server connection",
-        PS: "please remember to set up env vars in ./.env (example is in ./env.example",
-      })
-    }
-  })
+app.use('/api', Router({ db }))
 
-  // any requests that dont have an API path, map to react bundle
-  if (NODE_ENV === "production") {
-    const appBundleFolder = path.join(__dirname, '../../client/build') 
-    
-    app.use(express.static(appBundleFolder));
-    app.get('*', (req, res) => {
-      console.log("going to the app, not the API", path.join(appBundleFolder, 'index.html'))
-      res.sendFile(path.join(appBundleFolder, 'index.html'))
+// create a route for the app
+app.get('/api', (req, res) => {
+  if (NODE_ENV !== "production") {
+    res.json({
+      message: "this is my starter project for a Node.js API with a postgres server connection",
+      PS: "please remember to set up env vars in ./.env (example is in ./env.example",
     })
-  // when not in production, add a "ping" route
-  } else {
-    app.get('/api/ping', (req, res) => res.json("pong"))
   }
-  
-  // make the server listen to requests
-  app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}/`)
-  })
-
 })
+
+// any requests that dont have an API path, map to react bundle
+if (NODE_ENV === "production") {
+  const appBundleFolder = path.join(__dirname, '../../client/build') 
+  
+  app.use(express.static(appBundleFolder));
+  app.get('*', (req, res) => {
+    console.log("going to the app, not the API", path.join(appBundleFolder, 'index.html'))
+    res.sendFile(path.join(appBundleFolder, 'index.html'))
+  })
+// when not in production, add a "ping" route
+} else {
+  app.get('/api/ping', (req, res) => res.json("pong"))
+}
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`)
+})
+
+
+// })
