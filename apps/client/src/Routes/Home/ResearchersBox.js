870e583c4e3442cef2efc333acfb7565eaf2376a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-13 01:36:55 -0300

    Work on Home Page

---
+++ apps/client/src/Routes/Home/ResearchersBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Box from 'Components/Box'
+
+export const ResearchersBox = ({
+  ...props
+}) => {
+  return (
+    <Box>aaaaa</Box>
+  )
+}
+
+export default ResearchersBox
