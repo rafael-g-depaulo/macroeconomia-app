1dd56c438baaadbbd7626a21278582680475e7b3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-13 11:54:48 -0300

    ✨ Use markdown in about page

--- apps/client/src/Routes/Home/AboutBox.js
+++ apps/client/src/Routes/Home/AboutBox.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import Box from 'Components/Box'
 import Title from 'Components/Title'
-import Text from 'Components/Text'
 import { useAboutUs } from 'Api/aboutUs'
+import Markdown from 'Components/Markdown'
 
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
@@ -15,7 +15,7 @@ export const AboutBox = ({
   return (
     <Box>
       <Title>About Us</Title>
-      <Text>{ error?.toString() ?? data ?? lorem }</Text>
+      <Markdown source={ error?.toString() ?? data ?? lorem } />
     </Box>
   )
 }
