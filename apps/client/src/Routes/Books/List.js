84bd51a1fa6e80638ac01085e6d2e6f817bd4e7a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-18 15:34:23 -0300

    ✨ Connect client to API

--- apps/client/src/Routes/Books/List.js
+++ apps/client/src/Routes/Books/List.js
@@ -1,16 +1,33 @@
+import React from 'react'
+
+import { useAllBooks } from 'Api/books'
+
+import useUrl from 'Hooks/useUrl'
+
 import Wireframe from 'Components/Wireframe'
 import Container from 'Components/ContentContainer'
-import React from 'react'
 import Box from 'Components/Box'
+import Link from 'Components/CleanLink'
 
 export const List = ({
   ...props
 }) => {
+  const { data, isLoading } = useAllBooks()
+  const baseUrl = useUrl()
+
+  if (isLoading) return <div>loading...</div>
+
   return (
     <Wireframe>
       <Container>
         <Box>
-          aa
+          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+          {data.map(book => (
+            <div>
+              {/* <pre>{JSON.stringify(book, null, 2)}</pre> */}
+              <Link to={`${baseUrl}/${book.id}`}>view book {book.id}</Link>
+            </div>
+          ))}
         </Box>
       </Container>
     </Wireframe>
--- apps/client/src/Routes/Books/Show.js
+++ apps/client/src/Routes/Books/Show.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Wireframe from 'Components/Wireframe'
 import Container from 'Components/ContentContainer'
 import Box from 'Components/Box'
+import { useBook } from 'Api/books'
+import Link from 'Components/CleanLink'
 
 export const Show = ({
   id,
   ...props
 }) => {
+  const { data, isLoading } = useBook(id)
+  
+  if (isLoading) return <div>loading...</div>
+
   return (
     <Wireframe>
       <Container>
         <Box>
-          {id}
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <Link to=".">back</Link>
         </Box>
       </Container>
     </Wireframe>
