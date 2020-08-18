8e4205d116da20115fc3badc287f7e2d295e4bd6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-17 21:36:47 -0300

    🔥 Remove non-used papers components

--- apps/client/src/Routes/Papers/index.js
+++ apps/client/src/Routes/Papers/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import List from "./List"
-import Show from "./Show"
 
 export const Papers = ({
   match,
@@ -12,17 +11,10 @@ export const Papers = ({
   return (
     <Switch>
 
-      {/* page to list all researchers */}
+      {/* page to list all papers */}
       <Route exact path={`${path}`}>
         {() => (
           <List />
-        )}
-      </Route>
-
-      {/* page to show a single researcher */}
-      <Route path={`${path}/:id`}>
-        {({ match }) => (
-          <Show id={match.params.id}/>
         )}
       </Route>
 
