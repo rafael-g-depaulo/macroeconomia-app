269221c887f69afc41b6df0f218d3d2fe92c6064
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-24 00:14:27 -0300

    ✨ Add table to video list

--- apps/client/src/Routes/Videos/List.js
+++ apps/client/src/Routes/Videos/List.js
@@ -1,20 +1,89 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import MuiTableCell from '@material-ui/core/TableCell'
+import MuiTableRow from '@material-ui/core/TableRow'
 
 import { useAllVideos } from 'Api/video'
+import { tableBorderGreen } from 'Themes/default'
 
-import Box from 'Components/Box'
 import LazyPage from 'Components/LazyPage'
 import Loading from 'Components/Loading'
+import Table from 'Components/Table'
+import ExternalLink from 'Components/Link'
+import Box from 'Components/Box'
+
+const TableCell = styled(MuiTableCell)` 
+  &:not(:first-child) {
+    border-left: 2px solid ${tableBorderGreen};
+  }
+`
+
+const VideosTable = ({
+  data = [],
+}) => (
+  
+  <Table
+    columns={["Title", "Release Date"]}
+    data={data}
+    RowComponent={({ row }) => 
+      <MuiTableRow key={row.id}>
+        {row?.values?.map((value, i) => {
+          console.log("row", row)
+          return i === 0 ?
+          <TableCell
+            key={i}
+            align="inherit"
+            children={<ExternalLink download={row.download} href={row.link}>{value}</ExternalLink>}
+          />
+          :
+          <TableCell
+            key={i}
+            align="right"
+            children={value}
+          />
+        }
+        )}
+      </MuiTableRow>
+    }
+  />
+)
 
 export const List = ({
   ...props
 }) => {
   const { data, isLoading, error } = useAllVideos()
 
+  const formattedData = data
+    ?.map(({ id, title, youtube_link, file = [], created_at }) => {
+      
+      const { groups } = /(?<year>\d+)-(?<month>\d+)-(?<day>\d+)/.exec(created_at)
+      const date = `${groups.day}/${groups.month}/${groups.year}`
+
+      const fileMedia = file[0]?.url
+
+      const hasYoutubeLink = !!youtube_link
+
+      // console.log(title, youtube_link, fileMedia, date)
+      console.log(file[0]?.name);
+
+      return ({
+        id,
+        link: hasYoutubeLink ? youtube_link : fileMedia ?? "#",
+        download: !hasYoutubeLink,
+        // download: !hasYoutubeLink && file[0]?.name,
+        values: [
+          title,
+          date,
+        ]
+      })
+    })
+  
   return (
-    <LazyPage isLoading={true || isLoading || error} fallback={<Box><Loading /></Box>}>
+    <LazyPage isLoading={isLoading || error} fallback={<Box><Loading /></Box>}>
       <Box>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        <VideosTable data={formattedData}/>
       </Box>
     </LazyPage>
   )
