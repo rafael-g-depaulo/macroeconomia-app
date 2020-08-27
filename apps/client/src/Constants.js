6e437ade04acde2db7b67a7fdc44c4fa982ab24e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-27 09:01:58 -0300

    ✨ Add default aboutUs text

---
+++ apps/client/src/Constants.js
@@ -0,0 +1,39 @@
+export const aboutUs = `*Structuralist Development Macroeconomics* is defined by Bresser-Pereira, Oreiro and
+Marconi (2014, p.56) as “the economic theory that explains economic development as a
+historical process of capital accumulation, incorporating technological progress and
+structural change, in which accumulation depends on the existence of profitable
+investment opportunities offered by sustained growth of demand, which, in tum,
+depends on the balanced increase of the domestic market and of exports, which, finally,
+depends on the exchange rate to fluctuate around the equilibrium, instead of being
+chronically and cyclically overvalued, as is often the case in developing countries” 
+see: [http://bresserpereira.sitepessoal.com/documento/4900](http://bresserpereira.sitepessoal.com/documento/4900)
+
+The research group was founded in 2008 by José Luis Oreiro, Professor of Economics
+Department at University of Brasília, Brazil. Some researchers from the research group
+have been doing joint work for a long period, many of them published in economic
+journals in Brazil and abroad such as the *Brazilian Journal of Political Economy,
+Metroeconomica, Structural Change and Economic Dynamics, Review of Political
+Economy, Cepal Review, Investigación Económica, Journal of Post Keynesian
+Economics, Cambridge Journal of Economics, International Review of Applied
+Economics*, among others. It is worth noting the high productivity of the group
+members, many of whom are Cnpq level I researchers. Within the scope of this research
+group, the book &quot;Developmental Macroeconomics: new developmentalism as a growth
+strategy&quot; was organized published by Routledge, UK, in 2015. Also noteworthy is the
+publication of the book &quot;Moeda e Sistema Financeiro: ensaios em homenagem a
+Fernando Cardim de Carvalho&quot; in 2019.
+
+The research group is registered at Research Groups Directory of the Council for
+Technological and Scientific Research (CNPq) of the Brazilian Government (Diretório
+dos Grupos de Pesquisa do CNPQ) and certified by the University of Brasília (UnB).
+The address of the group in the directory can be seen at
+[http://dgp.cnpq.br/dgp/espelhogrupo/11697](http://dgp.cnpq.br/dgp/espelhogrupo/11697). José Luis Oreiro is the Leader of the
+research group and Luiz Fernando de Paula is the vice-leader.
+
+The group has six research lines:
+
+1. Growth and Distribution in Post Keynesian Models
+2. Growth, Infrastructure and Convergence Clubs.
+3. Financial Fragility and Business Cycles.
+4. Developmental Macroeconomics
+5. Stock-Flow Consistent Models
+6. Monetary Policy, Exchange Rate Regime and Sustainability of Public Debt.`
--- apps/client/src/Routes/Home/AboutBox.js
+++ apps/client/src/Routes/Home/AboutBox.js
@@ -5,7 +5,7 @@ import Title from 'Components/Title'
 import { useAboutUs } from 'Api/aboutUs'
 import Markdown from 'Components/Markdown'
 
-const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+import { aboutUs } from "Constants"
 
 export const AboutBox = ({
   ...props
@@ -15,7 +15,7 @@ export const AboutBox = ({
   return (
     <Box area="aboutus" {...props}>
       <Title>About Us</Title>
-      <Markdown source={ error?.toString() ?? data ?? lorem } />
+      <Markdown source={ error?.toString() ?? data ?? aboutUs } />
     </Box>
   )
 }
