4f677ce6e67b31e4c48d24ed5e62d47d7af66b79
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-27 12:49:57 -0300

    .

--- apps/client/src/index.js
+++ apps/client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { initialize } from './googleAnalytics'
+// import { initialize } from './googleAnalytics'
 
-initialize()
+// initialize()
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
