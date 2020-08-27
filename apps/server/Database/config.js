a7607f5d42e9605a39f9620339921881c76f1d5f
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-27 20:57:20 -0300

    🔧 Get db configs from DATABASE_URL

--- apps/server/Database/config.js
+++ apps/server/Database/config.js
@@ -1,12 +1,26 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+const databaseUrl = process.env.DATABASE_URL ?? ""
+const dbUriRegex = /^(?<client>postgres):\/\/(?<username>\w+):(?<password>\w+)@(?<host>.+):(?<port>\d+)\/(?<database>\w+)$/
+const result = dbUriRegex.exec(databaseUrl)
+
+const {
+  client   = process.env.DB_CLIENT ?? "postgres",
+  host     = process.env.DB_HOST   ?? "localhost",
+  username = process.env.DB_USER   ?? "user",
+  password = process.env.DB_PASS   ?? "PASSWORD_HERE",
+  port     = process.env.DB_PORT   ?? 5432,
+  database = process.env.DB_NAME   ?? "test",
+} = result?.groups ?? {}
+
 const config = {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  dialect: client,
+  host,
+  port,
+  username,
+  password,
+  database,
   define: {
     timestamps: true,
     underscored: true,
