fb4e925cf6913906caad62fc700199ae0ca50da1
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-08 18:55:00 -0300

    Move libs to routing/libs

---
+++ libs/routing/src/lib/routing.tsx
@@ -0,0 +1,18 @@
+import { lazy, LazyExoticComponent, Suspense } from 'react'
+import Loading from './Loading'
+
+export const lazyRoute = <T extends () => JSX.Element>(
+  moduleCallback: () => Promise<{ default: T }>,
+) => {
+  const RoutePage = lazy(moduleCallback) as LazyExoticComponent<
+    () => JSX.Element
+  >
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <RoutePage />
+    </Suspense>
+  )
+}
+
+export { Loading } from './Loading'
