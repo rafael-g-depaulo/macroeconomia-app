8b3fab3b3b28df0456c9df6114c5c5f97c9c1a29
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-08 12:12:32 -0300

    Generate routing library. Command run in body
    Ran command `nx g @nx/react:library --directory=libs --name=routing --importPath=@libs/routing --minimal=true`

---
+++ libs/src/lib/routing.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledRouting = styled.div`
+  color: pink;
+`;
+export function Routing() {
+  return (
+    <StyledRouting>
+      <h1>Welcome to Routing!</h1>
+    </StyledRouting>
+  );
+}
+
+export default Routing;
