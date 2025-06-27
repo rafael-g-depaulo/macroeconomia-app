5188b1b522c2c45d2374a73b64806a387167cd25
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-27 17:04:51 -0300

    Fix Markdown error crash

--- libs/components/src/Markdown.tsx
+++ libs/components/src/Markdown.tsx
@@ -5,6 +5,10 @@ import Text from '@components/Text'
 import Link from '@components/Link'
 import Image from '@components/Image'
 
+// this fixes an issue with ReactMarkdown trying to acess process.cwd.
+window.process ??= {} as any
+window.process.cwd = () => ''
+
 export const Markdown = ({ source = '', ...props }) => {
   return (
     <ReactMarkdown
