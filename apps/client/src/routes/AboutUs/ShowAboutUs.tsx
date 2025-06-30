59779a57ebf05a49cc7cbf812bcefe45ebae3341
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-30 20:10:35 -0300

    Add about us

--- apps/client/src/routes/AboutUs/ShowAboutUs.tsx
+++ apps/client/src/routes/AboutUs/ShowAboutUs.tsx
@@ -1,5 +1,12 @@
+import LazyPage from '@components/LazyPage'
+import AboutBox from '../Home/AboutBox'
+
 export const Component = () => {
-  return <></>
+  return (
+    <LazyPage>
+      <AboutBox maxWidth="900px" />
+    </LazyPage>
+  )
 }
 
 export default Component
