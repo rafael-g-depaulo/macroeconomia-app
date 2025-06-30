80fa5e9c4b46bbd95ebba53e8eaa56542dfcac5c
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-30 19:45:22 -0300

    Add routes

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,25 +1,54 @@
 import React, { ReactNode } from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
 import { lazyRoute } from '@libs/routing'
 
-export const router = createBrowserRouter([
-  {
-    id: 'home',
-    path: '/',
-    element: lazyRoute(() => import('./Home')),
-  },
-  {
-    id: 'researchers',
-    path: '/researchers',
-    children: [
-      {
-        path: '/',
-        element: lazyRoute(() => import('./Researchers/ListResearchers')),
-      },
-      {
-        path: '/:id',
-        element: lazyRoute(() => import('./Researchers/ShowResearcher')),
-      },
-    ],
-  },
-])
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={lazyRoute(() => import('./Home'))} />
+
+      <Route path="/researchers">
+        <Route
+          index
+          element={lazyRoute(() => import('./Researchers/ListResearchers'))}
+        />
+        <Route
+          path=":id"
+          element={lazyRoute(() => import('./Researchers/ShowResearcher'))}
+        />
+      </Route>
+
+      <Route
+        path="/about-us"
+        element={lazyRoute(() => import('./AboutUs/ShowAboutUs'))}
+      />
+
+      <Route
+        path="/events"
+        element={lazyRoute(() => import('./Events/ListEvents'))}
+      />
+
+      <Route
+        path="/papers"
+        element={lazyRoute(() => import('./Papers/ListPapers'))}
+      />
+
+      <Route
+        path="/videos"
+        element={lazyRoute(() => import('./Videos/ListVideos'))}
+      />
+
+      <Route path="/books">
+        <Route index element={lazyRoute(() => import('./Books/ListBooks'))} />
+        <Route
+          path=":id"
+          element={lazyRoute(() => import('./Books/ShowBook'))}
+        />
+      </Route>
+    </Route>,
+  ),
+)
