bc1aac37bfad4044fa56e372312913c5bc88c5ac
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-06-30 21:01:47 -0300

    Update typing of strapiGet

--- libs/api/src/strapiGet.ts
+++ libs/api/src/strapiGet.ts
@@ -1,8 +1,30 @@
 import { AxiosInstance, AxiosResponse } from 'axios'
-import {} from '@strapi/strapi'
+
+type UseAttributesIfExists<T> = T extends { attributes: infer T_ }
+  ? T_ & { id: string }
+  : T extends any
+    ? T
+    : never
+
+type StrapiSingleType<T> =
+  unknown extends UseAttributesIfExists<T> ? never : UseAttributesIfExists<T>
+
+type RemoveUnkown<T> = T extends never[]
+  ? never
+  : T extends (infer _T)[]
+    ? RemoveUnkown<_T>[]
+    : unknown extends T
+      ? never
+      : T
 
 export const strapiGet =
   <T>(key: string) =>
   (strapiInstance: AxiosInstance) =>
   () =>
-    strapiInstance.get<AxiosResponse<T>>(key).then(({ data }) => data.data)
+    strapiInstance
+      .get<
+        T extends { values: infer T_ }[]
+          ? AxiosResponse<RemoveUnkown<StrapiSingleType<T_>[]>>
+          : AxiosResponse<RemoveUnkown<StrapiSingleType<T>>>
+      >(key)
+      .then(({ data }) => data.data)
